3b4299d4cdfc7eca3a089f87ae94cc70dc709435
Mohamed Farghaly <m.farghaly.ext@Tahaluf.ae>
2024-05-30 15:38:29 +0300

    Added JobCandidatesController implementation - AddOrUpdateCandidate api

---
+++ src/WebApi/Controllers/JobCandidatesController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dtos;
+using Application.JobCandidate.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class JobCandidatesController(IMediator _mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AddOrUpdateCandidate([FromBody] JobCandidateDto candidateDTO, CancellationToken cancellationToken = default)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var command = new AddOrUpdateJobCandidateCommand(candidateDTO);
+
+        return Ok(await _mediator.Send(command, cancellationToken));
+    }
+}
